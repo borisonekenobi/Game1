6ed30cd15a80830af4fecdd8ed891ce053467161
Zur3l <1allendan3@gmail.com>
2024-03-02 20:47:20 -0500

    b

--- Player.gd
+++ Player.gd
@@ -1,12 +1,11 @@
 extends CharacterBody3D
 
-
 const SPEED = 5.0
 const JUMP_VELOCITY = 4.5
+var handheld: Node3D
 
 # Get the gravity from the project settings to be synced with RigidBody nodes.
 var gravity = ProjectSettings.get_setting("physics/3d/default_gravity")
-
 
 func _physics_process(delta):
 	# Add the gravity.
@@ -22,18 +21,30 @@ func _physics_process(delta):
 	
 	if holding_left and !holding_right:
 		velocity.x = -SPEED
-	if holding_right and !holding_left:
+	elif holding_right and !holding_left:
 		velocity.x = SPEED
-	
-	# Get the input direction and handle the movement/deceleration.
-	# As good practice, you should replace UI actions with custom gameplay actions.
-	#var input_dir = Input.get_vector("ui_left", "ui_right", "ui_up", "ui_down")
-	#var direction = (transform.basis * Vector3(input_dir.x, 0, input_dir.y)).normalized()
-	#if direction:
-		#velocity.x = direction.x * SPEED
-		#velocity.z = direction.z * SPEED
-	#else:
-		#velocity.x = move_toward(velocity.x, 0, SPEED)
-		#velocity.z = move_toward(velocity.z, 0, SPEED)
+	else:
+		velocity.x = 0
 
 	move_and_slide()
+
+func _input(event):
+	# Mouse in viewport coordinates.
+	if event is InputEventMouseButton:
+		pass
+		#print("Mouse Click/Unclick at: ", event.position)
+	elif event is InputEventMouseMotion:
+		var playerPos = $Camera3D.unproject_position(position)
+		var mousePos = event.position
+		var viewportSize = get_viewport().get_visible_rect().size
+		mousePos -= playerPos
+		
+		handheld.rotation.z = Vector2(0, 1).angle_to_point(mousePos)
+		print("Mouse Motion at: ", event.position)
+		print("pos at: ", $Camera3D.unproject_position(position))
+
+	# Print the size of the viewport.
+	#print("Viewport Resolution is: ", get_viewport().get_visible_rect().size)
+
+func _ready():
+	handheld = get_node("Handheld")
